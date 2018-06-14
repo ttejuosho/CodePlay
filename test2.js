ed4abae8b48248101427312e6d89cb24d7ad7bc3
ttejuosho <theycallmeflowz@yahoo.com>
2018-06-13 19:48:40 -0500

    2018 tax codes implemented

--- test2.js
+++ test2.js
@@ -1,42 +1,61 @@
 $(document).ready(function(){
     console.log("SYSTEM READY");
-    let entryForm = $('#deductionsDiv');
 
     $("#results").hide();
-    function calculateSalary(hourlyRate, hoursWorked){
+    function calculateSalary(hourlyRate, hoursWorked, allowanceClaimed){
         let overtimeHours = (hoursWorked - 40);
         let overTimePay = (overtimeHours*hourlyRate*1.5);
         let weeklyFTPay = (hourlyRate*40);
         let weeklyPay = (weeklyFTPay + overTimePay);
+        let withheldAmount = (allowanceClaimed*79.8).toFixed(2);
+        let amtSubjectToWithholding = (weeklyPay - withheldAmount).toFixed(2);
         let biWeeklyPay = (weeklyPay*2).toFixed(2);
         let monthlyPay = (biWeeklyPay*2).toFixed(2);
         let annualPay = (weeklyFTPay*52).toFixed(2);
-        let taxRate = 0;
-
+        let weeklyTax = 0;
+        
 
     // Choose Tax Brackets based on Annual Pay to calculate Taxes
-    if (annualPay < 9325){
-        taxRate = 0.1;
-    } else if (annualPay > 9325 && annualPay < 37950){
-        taxRate = 0.15;
-    } else if (annualPay > 37950 && annualPay < 91900){
-        taxRate = 0.25;
-    } else if (annualPay > 91900 && annualPay < 191650){
-        taxRate = 0.28;
-    } else if (annualPay > 191650 && annualPay < 416700){
-        taxRate = 0.33;
-    }  else if (annualPay > 416700 && annualPay < 418400){
-        taxRate = 0.35;
+    if ( amtSubjectToWithholding > 71 && amtSubjectToWithholding < 254 ){
+        let taxableIncome = (amtSubjectToWithholding - 71);
+            weeklyTax = taxableIncome*0.1;
+    } else if ( amtSubjectToWithholding > 254  && amtSubjectToWithholding < 815 ){
+        let taxableIncome = (amtSubjectToWithholding - 254);
+            weeklyTax = (taxableIncome*0.12) + 18.30;
+    } else if ( amtSubjectToWithholding > 815  && amtSubjectToWithholding < 1658 ){
+        let taxableIncome = (amtSubjectToWithholding - 815);
+            weeklyTax = (taxableIncome*0.22) + 85.62;
+    } else if ( amtSubjectToWithholding > 1658  && amtSubjectToWithholding < 3100 ){
+        let taxableIncome = (amtSubjectToWithholding - 1658);
+            weeklyTax = (taxableIncome*0.24) + 271.08;
+    } else if ( amtSubjectToWithholding > 3100  && amtSubjectToWithholding < 3917 ){
+        let taxableIncome = (amtSubjectToWithholding - 3100);
+            weeklyTax = (taxableIncome*0.32) + 617.16;
+    }  else if ( amtSubjectToWithholding > 3917  && amtSubjectToWithholding < 9687 ){
+        let taxableIncome = (amtSubjectToWithholding - 3917);
+            weeklyTax = (taxableIncome*0.35) + 878.6;
     } else {
-        taxRate = 0.396;
+        let taxableIncome = (amtSubjectToWithholding - 9687);
+            weeklyTax = (taxableIncome*0.37) + 2898.10;
     }
 
+
+    console.log(weeklyTax);
+    let weeklyPayAfterTax = (weeklyPay - weeklyTax).toFixed(2);
+    let biWeeklyPayAfterTax = (weeklyPayAfterTax*2).toFixed(2);
+    let monthlyPayAfterTax = (biWeeklyPayAfterTax*2).toFixed(2);
+    let annualPayAfterTax = (monthlyPayAfterTax*12).toFixed(2);
+    let biWeeklyTax = (weeklyTax*2).toFixed(2);
+    let monthlyTax = (biWeeklyTax*2).toFixed(2);
+    let annualTax = (monthlyTax*12).toFixed(2);
     let workStatus = "Full Time";
-    let annualTax = (taxRate*annualPay).toFixed(2);
-    let biWeeklyTax = (biWeeklyPay*taxRate).toFixed(2);
-    let annualPayAfterTax = (annualPay - annualTax).toFixed(2);
-    let biWeeklyPayAfterTax = (biWeeklyPay - biWeeklyTax).toFixed(2); 
-    let taxRatePercent = (taxRate*100).toFixed(2);
+
+    
+    // let annualTax = (taxRate*annualPay).toFixed(2);
+    // let biWeeklyTax = (biWeeklyPay*taxRate).toFixed(2);
+    // let annualPayAfterTax = (annualPay - annualTax).toFixed(2);
+    // let biWeeklyPayAfterTax = (biWeeklyPay - biWeeklyTax).toFixed(2); 
+    // let taxRatePercent = (taxRate*100).toFixed(2);
 
     // set work status in report
     if (hoursWorked < 40){  workStatus = "Part Time" }
@@ -44,9 +63,8 @@ $(document).ready(function(){
     // Render result to web page
     $("#results").html(`
         If you earn $${hourlyRate}/hour and you work ${workStatus} (${hoursWorked} hours),<br> 
-        Bi-Weekly pay: $${biWeeklyPay}, <br>
-        Monthly Pay: $${monthlyPay}, <br>  
-        At a tax rate of ${taxRatePercent}%, <br> 
+        Bi-Weekly Gross Pay: $${biWeeklyPay}, <br>
+        Monthly Gross Pay: $${monthlyPay}, <br>  
         You'll get approximately $${biWeeklyPayAfterTax} every 2 weeks <br> 
         Annual Pay: $${annualPay}<br>
         And you'll get $${annualPayAfterTax} yearly after tax.
@@ -59,8 +77,9 @@ $(document).ready(function(){
         // Save input values in variables
         let hoursWorked = $("#hoursWorked").val();
         let hourlyRate = $("#hourlyRate").val();
+        let allowanceClaimed = $('#allowanceClaimed').val();
         let deduction = $('.deduction').val();
-        console.log(deduction);
+
 
         if (hourlyRate === ""){
             $("#hourlyRate").toggleClass("animated shake");
@@ -72,11 +91,13 @@ $(document).ready(function(){
         $("#results").show();
 
         // Call function to calculate income
-        calculateSalary(hourlyRate, hoursWorked);
-
+        calculateSalary(hourlyRate, hoursWorked, allowanceClaimed);
+        console.log(deduction);
+        
         // Clear form fields
         $("#hoursWorked").val("");
         $("#hourlyRate").val("");
+        $('#allowanceClaimed').val("");
         $('.deduction').val("");
     }
 
@@ -84,13 +105,14 @@ $(document).ready(function(){
 
 // Set Listener on deductions link to create form for deductions
 $(".addInputBox").on("click", function (){
+    let entryForm = $('#deductionsDiv');
     let newInputDiv = $('<div>').addClass('col');
     let beforeTaxCheckDiv = $('<div>').addClass('form-check');
     let afterTaxCheckDiv = $('<div>').addClass('form-check');
     let beforeTaxLabel = $('<label>').addClass('form-check-label').text('Before Tax');
     let afterTaxLabel = $('<label>').addClass('form-check-label').text('After Tax');
     let deductionInput = $('<input>').addClass('form-control deduction').attr('placeholder', 'Enter Deduction %');
-    let beforeTaxInput = $('<input>').addClass('form-check-input beforeTax').attr('type', 'checkbox');
+    let beforeTaxInput = $('<input>').addClass('form-check-input beforeTax disabled').attr('type', 'checkbox');
     let afterTaxInput = $('<input>').addClass('form-check-input afterTax').attr('type', 'checkbox');
     let beforeTaxCheckBox = beforeTaxCheckDiv.append(beforeTaxInput).append(beforeTaxLabel);  
     let afterTaxCheckBox = afterTaxCheckDiv.append(afterTaxInput).append(afterTaxLabel);
