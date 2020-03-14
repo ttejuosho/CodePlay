13e98694ed3473afc9db4f3088a56bd4566e35bb
Taiwo <ttejuosho@outlook.com>
2020-03-13 22:10:49 -0500

    print fibonacci series

--- cmdline.js
+++ cmdline.js
@@ -17,8 +17,30 @@ function buildString(arg1){
     return str;
 }
 
+function printFibonacci(arg1){
+    var output = [];
+    var series = "";
+    for (var i = 0; i < arg1; i++){
+        if (i <= 1){
+            output.push(1);
+            series += 1 + ", ";
+        } else {
+            output.push(output[output.length - 1] + output[output.length - 2]);
+            if (i == arg1 - 1){
+                series += (output[i - 1]) + (output[i - 2]) + ". ";
+            } else {
+                series += (output[i - 1]) + (output[i - 2]) + ", ";
+            }
+        }
+    }
+    //console.log(output);
+    console.log(series);
+}
+
 if (isInteger(arg1)){
-    console.log(buildString(arg1));
+    //console.log(buildString(arg1));
+    printFibonacci(arg1)
 } else {
     console.log("Invalid Argument Passed");
 }
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
