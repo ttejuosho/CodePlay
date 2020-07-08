7e44b6b42403ef08efc3f745b50c282a0ad80b87
Taiwo Tejuosho <taiwotejuosho@TaiMBP18.local>
2020-07-08 18:43:58 -0500

    more

--- cmdline.js
+++ cmdline.js
@@ -36,10 +36,28 @@ function getNextTerm(seriesArray){
 }
 
 
-if (isInteger(arg1)){
+// if (isInteger(arg1)){
     //console.log(buildString(arg1));
-    printFibonacciSeries(arg1);
-} else {
-    console.log("Invalid Argument Passed");
-}
+//     printFibonacciSeries(arg1);
+// } else {
+//     console.log("Invalid Argument Passed");
+// }
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+var xhr = new XMLHttpRequest();
+xhr.timeout = 5000;
+xhr.onreadystatechange = function(e){
+console.log(this);
+if (xhr.readyState === 4){
+    if (xhr.status === 200){
+    //$("#result").html(xhr.response);
+    console.log(xhr.response);
+    } else {
+    console.error("XHR didn't work: ", xhr.status);}
+    }
+}
+xhr.ontimeout = function (){
+console.error("request timedout: ", xhr);
+}
+xhr.open("get", "https://auctiondata.iaai.com/Search?AccessKey=faa3f7d8-932e-42bf-9a61-2b2cf379ad75", /*async*/ true);
+xhr.send();
