c4f7e234db6205a6e0fc31823ad6fe24628b9dba
Taiwo <ttejuosho@outlook.com>
2022-03-31 00:02:46 -0500

    SortedName Solutions Added

--- project-euler.js
+++ project-euler.js
@@ -1,3 +1,5 @@
+fs = require("fs");
+
 //console.log("This is Project Euler.");
 
 const path = require("path");
@@ -547,9 +549,11 @@ getAmicableNumbers = () => {
   console.log(getSum(amicableNumbers));
 };
 
-getAmicableNumbers();
+//getAmicableNumbers();
 //console.log(getFactors(220));
 
+//Problem 22
+//Name Scores
 let names = [
   "MARY",
   "PATRICIA",
@@ -5715,3 +5719,48 @@ let names = [
   "BRODERICK",
   "ALONSO",
 ];
+
+let sortedNames = names.sort();
+let alphabeticalValues = {
+  A: 1,
+  B: 2,
+  C: 3,
+  D: 4,
+  E: 5,
+  F: 6,
+  G: 7,
+  H: 8,
+  I: 9,
+  J: 10,
+  K: 11,
+  L: 12,
+  M: 13,
+  N: 14,
+  O: 15,
+  P: 16,
+  Q: 17,
+  R: 18,
+  S: 19,
+  T: 20,
+  U: 21,
+  V: 22,
+  W: 23,
+  X: 24,
+  Y: 25,
+  Z: 26,
+};
+
+nameScores = () => {
+  let nameScoreSum = 0;
+  for (let i = 0; i < sortedNames.length; i++) {
+    let nameArray = sortedNames[i].split("");
+    let nameScore = 0;
+    for (let j = 0; j < nameArray.length; j++) {
+      nameScore += alphabeticalValues[nameArray[j]];
+    }
+    nameScoreSum += nameScore * i;
+  }
+  console.log(nameScoreSum);
+};
+
+nameScores(); //870,873,746
